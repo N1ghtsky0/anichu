9a7189951512d48421346f239d4c44b3e36d7a5b
N1ghtsky0 <20korea01@gmail.com>
2023-12-12 13:12:43 +0900

    init commit

---
+++ src/test/java/io/anichu/anichu/AnichuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anichu.anichu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnichuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
