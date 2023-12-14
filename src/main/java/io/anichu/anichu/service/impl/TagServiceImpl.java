df07e96cfdb2b43c9a1d93bee66387ea9ed5902d
N1ghtsky0 <20korea01@gmail.com>
2023-12-14 13:05:45 +0900

    feat: 태그 검색 로직 수정

--- src/main/java/io/anichu/anichu/service/impl/TagServiceImpl.java
+++ src/main/java/io/anichu/anichu/service/impl/TagServiceImpl.java
@@ -2,12 +2,15 @@ package io.anichu.anichu.service.impl;
 
 import io.anichu.anichu.dto.response.GetAnimeSummaryResponseDTO;
 import io.anichu.anichu.dto.response.GetTagResponseDTO;
+import io.anichu.anichu.entity.AnimeTag;
 import io.anichu.anichu.repository.AnimeRepo;
 import io.anichu.anichu.repository.TagRepo;
-import io.anichu.anichu.repository.mapper.AnimeTagMapper;
 import io.anichu.anichu.service.CommentService;
 import io.anichu.anichu.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,8 +20,8 @@ import java.util.List;
 public class TagServiceImpl implements TagService {
     private final TagRepo tagRepo;
     private final AnimeRepo animeRepo;
-    private final AnimeTagMapper animeTagMapper;
     private final CommentService commentService;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public List<GetTagResponseDTO> getAllTags() {
@@ -27,8 +30,11 @@ public class TagServiceImpl implements TagService {
 
     @Override
     public List<GetAnimeSummaryResponseDTO> getAllAnimeByTag(String[] tags) {
-        return animeTagMapper.selectAllAnimeByTag(tags).stream()
-                .map(seq -> animeRepo.findById(seq).orElseThrow())
+        Criteria criteria = Criteria.where("tagArr").all((Object[]) tags);
+        Query query = new Query(criteria);
+
+        return mongoTemplate.find(query, AnimeTag.class).stream()
+                .map(animeTag -> animeRepo.findById(animeTag.getAnimeSeq()).orElseThrow())
                 .map(anime -> GetAnimeSummaryResponseDTO.from(anime, commentService.getAnimeAverageScore(anime.getSeq())))
                 .toList();
     }
