f2a23e89cc1cad2dcf429d30c1d4bdd027af9a44
N1ghtsky0 <20korea01@gmail.com>
2023-12-14 16:32:08 +0900

    feat: 제작사 추가 페이지

--- src/main/java/io/anichu/anichu/controller/AdminController.java
+++ src/main/java/io/anichu/anichu/controller/AdminController.java
@@ -1,10 +1,12 @@
 package io.anichu.anichu.controller;
 
+import io.anichu.anichu.dto.request.InsertCompanyRequestDTO;
 import io.anichu.anichu.service.ProductionCompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -17,6 +19,18 @@ public class AdminController {
     public String companyManagePage(Model model) {
         model.addAttribute("list", productionCompanyService.getAllProductionCompany());
         return "/admin/company/list";
+    }
+
+    @GetMapping("/company/new")
+    public String companyInsertPage() {
+        return "/admin/company/insert";
+    }
+
+    @PostMapping("/company/new")
+    public String companyInsertProcess(InsertCompanyRequestDTO requestDTO) {
+        productionCompanyService.insertProductCompany(requestDTO);
+
+        return "redirect:/admin/company";
     }
 
 }
