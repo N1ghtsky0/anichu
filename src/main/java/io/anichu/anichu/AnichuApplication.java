49b17c754783bf0af9a696f519d9e5bb85d9e6c3
N1ghtsky0 <20korea01@gmail.com>
2024-02-19 21:59:21 +0900

    first commit

---
+++ src/main/java/io/anichu/anichu/AnichuApplication.java
@@ -0,0 +1,13 @@
+package io.anichu.anichu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnichuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnichuApplication.class, args);
+	}
+
+}
