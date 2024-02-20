50cdeec8efa779cc17cd809e3637ba0047958ce4
N1ghtsky0 <20korea01@gmail.com>
2024-02-20 16:30:24 +0900

    feat: 시큐리티 정적 자원 ignore

--- src/main/java/io/anichu/anichu/config/SecurityConfig.java
+++ src/main/java/io/anichu/anichu/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package io.anichu.anichu.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,8 +16,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .requestMatchers(ALLOWED_URLS).permitAll()
+                .requestMatchers(HttpMethod.GET, "/anime", "/anime/**").permitAll()
                 .anyRequest().authenticated());
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer securityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
